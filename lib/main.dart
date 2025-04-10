239cf6fbf7fd5ce2ae0413200298b855b2a16085
Dopa91 <code.dopatka@gmail.com>
2025-04-10 23:55:07 +0200

    location

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:streams_examples_live/ip_address/ip_address.dart';
+import 'package:streams_examples_live/ip_address/ip_address_repository.dart';
 import 'package:streams_examples_live/location/generate_locations.dart';
 import 'package:streams_examples_live/location/location.dart';
 
+final IpAddressRepository repository = IpAddressRepository();
+
 void main() async {
   final locations = generateLocations();
+  final ipAddress = repository.generateIPAddresses();
 
   // await for (Location location in locations) {
   //   print(location);
@@ -13,6 +18,16 @@ void main() async {
       print("Neue Location \\o/ lat: ${l.latitude}, long: ${l.longitude}");
     },
   );
+
+  ipAddress.listen(
+    (actualIp) {
+      print("$actualIp");
+    },
+  );
+
+  await for (IpAddress ipAddress in ipAddress) {
+    print(ipAddress);
+  }
 
   //locations.listen(printLocation);
 }
